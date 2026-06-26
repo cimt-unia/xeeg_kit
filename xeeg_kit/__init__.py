a6da352143909c773c6096b1d038b24b1c71a9f1
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-06-26 17:47:22 +0200

    Update __init__.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

--- xeeg_kit/__init__.py
+++ xeeg_kit/__init__.py
@@ -2,12 +2,15 @@
 """
 xeeg_kit: Preprocessing toolkit for high-density EEG (e.g., BEL EEG System One).
 """
-from ._config import *  # Set environment variables first
+from ._config import * 
 
 from .bel_280 import parse_gpsc, create_montage_from_gpsc, BELStandardizer
 from .artifact_cleaning import execute_meegkit, execute_icalabel
 from .parallel import process_subjects_parallel, process_single_subject
 from .utils import detect_bad_channels, find_cleanest_segment
+
+# Analysis & Visualization Tools
+from .analysis import generate_bel_channel_map, select_channels, plot_comparison
 
 __version__ = "0.1.0"
 __author__ = "CIMT"
