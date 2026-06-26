212539235cf58c6af2bbd3118d29af1de0d4f6d0
ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>
2026-06-26 19:06:48 +0200

    Update __init__.py
    Signed-off-by: ジェイ・ラコマ <132283879+lacomaofficial@users.noreply.github.com>

--- xeeg_kit/__init__.py
+++ xeeg_kit/__init__.py
@@ -9,15 +9,21 @@ from .artifact_cleaning import execute_meegkit, execute_icalabel
 from .parallel import process_subjects_parallel, process_single_subject
 from .utils import detect_bad_channels, find_cleanest_segment
 
-# Analysis & Visualization Tools
-from .analysis import generate_bel_channel_map, select_channels, plot_comparison
+# Analysis Tools
+from .analysis import select_channels, plot_comparison
+
+# Visualization & Mapping Tools
 from .viz import (
     get_bel_rename_map,
     print_anatomical_summary,
-    create_bel_channel_map,
+    load_bel_channel_map,       
     plot_headset_3d,
     plot_bad_channels_3d
 )
+
+# Backward Compatibility
+generate_bel_channel_map = load_bel_channel_map
+create_bel_channel_map = load_bel_channel_map
 
 __version__ = "0.1.0"
 __author__ = "CIMT"
